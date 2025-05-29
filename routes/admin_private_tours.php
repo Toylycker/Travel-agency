b4e34188572a64ba5682a73f22fbd88c1e3b67d4
Toygeldi <toyly2712@gmail.com>
2025-05-30 00:58:08 +0800

    save state

--- routes/admin_private_tours.php
+++ routes/admin_private_tours.php
@@ -10,6 +10,5 @@ Route::middleware(['auth:sanctum', 'verified'])
         Route::get('private_tours/create', [PrivateTourController::class, 'create'])->name('private_tours.create');
         Route::post('private_tours', [PrivateTourController::class, 'store'])->name('private_tours.store');
         Route::get('private_tours/{tour}/edit', [PrivateTourController::class, 'edit'])->name('private_tours.edit');
-        Route::resource('private_tours', PrivateTourController::class)->only(['update']);
         Route::post('private_tours/{tour}/update', [PrivateTourController::class, 'update'])->name('private_tours.update');
     }); 
